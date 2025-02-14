d6b6033cc2170790cbb27c407b6d9220aa15e39a
Travis Trinidad <molotheepiceyego@gmail.com>
2025-02-14 00:28:48 -0800

    - almost done

--- main.cpp
+++ main.cpp
@@ -66,12 +66,80 @@ public:
         length--;
     }
 
-    void deleteNode(int index) {
-       //TODO:Write the function to delete at the given index. Reuse the pre-written functions for edge cases. Account for missing index.
+    void deleteNode(int targetindex) {
+        //if list is empty, print error message
+        if (head == NULL) {
+            cout << "empty list" << endl;
+            return;
+        }
+        //if targetIndex is 0, delete the first node and return
+        if (targetindex == 0) {
+            delfirst();
+            return;
+        }
+
+        //make a temporary pointer at the head so we can start at the top of list
+        Node<T> *temp = head;
+        int indexCounter = 0;
+
+        //traverse the list to find the node before targetindex
+        while (temp->next != NULL && indexCounter < targetindex - 1) {
+            temp = temp->next;
+            indexCounter++;
+        }
+
+        //if targetIndex is out of bounds
+        if (temp->next == NULL) {
+            cout << "index out of bounds" << endl;
+            return;
+        }
+
+        //store the node to be deleted
+        Node<T> *nodeToDelete = temp->next;
+
+        //update next pointer to skip the deleted node
+        temp->next = nodeToDelete->next;
+
+        //delete the target node
+        delete nodeToDelete;
+
+        //decrement length
+        length--;
     }
+
 
    void insert(int index, T *value) {
         //TODO:Write a function to insert a new node at a give index. Reuse the pre-written functions for edge cases. Account for missing index
+        Node<T> *newNode = new Node<T>(value); //assinging a node to *value
+        Node<T> *temp = head;
+        int indexCounter = 0;
+
+        //if list is empty, print error message
+        if (head == NULL) {
+            cout << "empty list" << endl;
+            return;
+        }
+        //if targetIndex is 0, set head to newNode
+        if (index == 0) {
+            head = newNode;
+        }
+
+        //go through the list until we reach a node after our index
+        while (temp->next != NULL && indexCounter < index - 1) //will land right before target index
+        {
+            temp = temp->next;
+            indexCounter++;
+        }
+
+        if (temp->next == NULL)
+        {
+            cout << "index out of bounds" << endl;
+        }
+
+        newNode->next = temp->next; //the new node will point to what the previous node was pointing to. ex) previous being x->y, now newNode->y
+        temp->next = newNode; //since the previous has nothing to point to, we make the previous node point to the new node.
+        length++;
+
     }
 
    void reverselist(){
@@ -94,10 +162,8 @@ int main() {
     student *s3 = new student("C", 22);
     LinkedList<student> *ll = new LinkedList<student>(s1);
     ll->add(s2);
-    ll->addhead(s3);
+    ll->add(s3);
     ll->print();
-    ll->delfirst();
-    ll->print();
-    ll->dellast();
+    ll->insert(0, s2);
     ll->print();
 }
